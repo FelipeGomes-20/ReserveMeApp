0376d5ae2eab811301df6cb8b43228faea03860f
Felipe Rosa Gomes <584021008@PMSBC.GOV.BR>
2025-03-17 17:53:01 -0300

    First Commit

---
+++ src/main/java/com/fiap/reserveme/ReserveMeAppApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.reserveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveMeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveMeAppApplication.class, args);
+	}
+
+}
